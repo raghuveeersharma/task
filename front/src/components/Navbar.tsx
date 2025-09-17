0b89881dfa04ca2ef8798c371c22233db66dea2a
raghuveersharma <sharmaraghu157@gmail.com>
2025-09-17 14:05:35 +0530

    add dropdown and gihub link

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -1,10 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import ThemeSelector from "./ThemeSelector.js";
+
 const Navbar = () => {
+  const navigate = useNavigate();
+  console.log("navbar");
   return (
-    <div className=" h-16 flex items-center bg-green-500 px-10 text-white shadow-lg shadow-gray-300">
-      <div className="mx-auto w-[90%]">
-        <div className="flex justify-between gap-[80%] ">
-          <div className="text-3xl tracking-wider font-bold">DataWala</div>
-          <div className="underline text-xl link">Github</div>
+    <div className=" h-16 w-[100%] flex items-center navbar bg-success  shadow-gray-300">
+      <div className="mx-auto w-[80%]">
+        <div className="flex justify-between lg:gap-80 gap-10 md:gap-40 ">
+          <div
+            className="md:text-3xl text-xl tracking-wider font-bold"
+            onClick={() => navigate("/")}
+          >
+            DataWala
+          </div>
+          <div>
+            <ThemeSelector />
+          </div>
+          <a
+            href="https://github.com/raghuveeersharma/task.git"
+            target="__blank"
+            className="btn btn-soft btn-accent md:text-xl text-lg"
+          >
+            <span>Github</span>
+          </a>
         </div>
       </div>
     </div>
