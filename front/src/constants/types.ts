77db8a6bb5d2146feb3637284943a74c4fc2ddfd
raghuveersharma <sharmaraghu157@gmail.com>
2025-09-17 11:46:37 +0530

    create a separte file for types and interface

--- front/src/constants/types.ts
+++ front/src/constants/types.ts
@@ -1,5 +1,49 @@
-export type user={
-  id:string,
-  name:string,
+export interface User{
+  id:string
+  name:string
+  last_name:string
   age:number|null
+  status?:boolean
 }
+export interface Counts {
+  teen: number;
+  adult: number;
+  old: number;
+};
+
+export type TableRowProps = {
+  data: User;
+  fullName?:string;
+  index: number;
+  handelDelete: (id: string) => void;
+  setBlur: () => void;
+  handelUpdate: (
+    id: string,
+    user: { name: string; age: number | null }
+  ) => void;
+};
+
+export type DetailBoxProps = {
+  id: string;
+  namee: string;
+  agee: number | null;
+  open: () => void;
+  handelDelete: (id: string) => void;
+  handelUpdate: (
+    id: string,
+    user: { name: string; age: number | null }
+  ) => void;
+};
+export type AddUsersProps = {
+  open: () => void;
+  handelAdd: (data: { id: string; name: string; age: number | null }) => void;
+  setBlur: (s: boolean) => void;
+};
+
+export interface Theme{
+  name:string,
+  label:string,
+  colors:[string,string,string,string]
+
+}
+
